476a885a3e18acf9ed8612f78aff2ed7ad809636
Prasad <sameer.deshmukh.6655@gmail.com>
2022-01-22 18:03:17 +0530

    addition and subtraction of two numbers

--- framework/base.py
+++ framework/base.py
@@ -1,1 +1,19 @@
 # TODO : Add Open Chrome code
+class Addition:
+    """  addition of 2 numbers   """
+
+    def add_two_num(self, num1, num2):
+        print(num1 + num2)
+
+
+obj = Addition()
+obj.add_two_num(44, 678)
+
+""" subtraction of 2 numbers"""
+
+
+def subs_two_num(num1, num2):
+    print(num1 - num2)
+
+
+subs_two_num(10, 5)
